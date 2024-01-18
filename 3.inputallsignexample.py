13914d865ccdadade2cb2f7ba20d64c9174e8b5e
Hariprasaath2002 <hariprasaath2002@gmail.com>
2024-01-18 12:21:53 +0530

    python

---
+++ 3.inputallsignexample.py
@@ -0,0 +1,8 @@
+a =float( input("enter the number:" ))
+b = float(input("enter the number:" ))
+print("your value for add =", a +b)
+print("your value for sub=", a -b)
+print("your value for multi=", a*b)
+print("your value for division=", a/b)
+
+
